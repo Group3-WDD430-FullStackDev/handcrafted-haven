a7f7b383e15fc88205fb8b522561d54420798ac0
Alvaro <76889519+alvarobgodoy@users.noreply.github.com>
2025-01-30 14:43:45 +0200

    update: styling and structure of header and navigation

--- src/components/Common/Header.tsx
+++ src/components/Common/Header.tsx
@@ -3,72 +3,73 @@
 import Image from "next/image";
 import Link from "next/link";
 import { UserIcon } from "@heroicons/react/24/solid";
-import SearchBar from "../SearchBar";
 import { useState } from "react";
 import { useSession, signIn, signOut } from "next-auth/react";
+import SearchBar from "../SearchBar";
 
 export default function Header() {
   const { data: session, status } = useSession();
   const [isProfileOpen, setIsProfileOpen] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   if (status === "loading") {
-    return <div>Loading...</div>;
+    return (
+      <header className="w-full bg-white border-gray-200 dark:bg-gray-900">
+          <nav className="max-w-6xl mx-auto bg-white border-gray-200 dark:bg-gray-900">
+            <div className="max-w-screen-xl flex items-center justify-between mx-auto p-4">Loading...</div>
+          </nav>
+      </header>
+    );
   }
 
-  console.log("Header [Session]", session);
-
   return (
-    <header className="w-full">
-      <nav className="bg-white border-gray-200 dark:bg-gray-900">
+    <header className="w-full bg-white border-gray-200 dark:bg-gray-900">
+      <nav className="max-w-6xl mx-auto bg-white border-gray-200 dark:bg-gray-900">
         <div className="max-w-screen-xl flex items-center justify-between mx-auto p-4">
           {/* Logo */}
-          <Link
-            href="/"
-            className="flex items-center space-x-3 rtl:space-x-reverse"
-          >
-            <Image
-              src="/logo.png"
-              alt="HancraftedHaven Logo"
-              width={100}
-              height={40}
-            />
+          <Link href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
+            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+              HancraftedHaven
+            </span>
           </Link>
+
           {/* Middle Section (SearchBar) */}
-          <div className="flex-1 flex justify-center">
+          <div className="flex-1 flex justify-center max-w-[500px] mx-auto">
             <SearchBar />
           </div>
+
           {/* Right Section (Profile & Hamburger) */}
-          <div className="flex items-center space-x-3 rtl:space-x-reverse">
+          <div className="flex items-center md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
             {session ? (
               // Authenticated User - Show Profile Button
               <>
                 <button
-                  type="button"
                   onClick={() => setIsProfileOpen(!isProfileOpen)}
-                  className="relative flex items-center justify-center w-10 h-10 bg-accent hover:bg-[var(--accent-hover)] rounded-full focus:ring-4 focus:ring-[var(--accent)] dark:focus:ring-[var(--accent)] transition duration-200"
-                  aria-expanded={isProfileOpen}
+                  type="button"
+                  className="flex text-sm bg-gray-800 rounded-full md:me-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
+                  aria-expanded={isProfileOpen ? "true" : "false"}
                 >
                   <span className="sr-only">Open user menu</span>
                   {session.user?.image ? (
                     <Image
-                      className="w-full h-full rounded-full object-cover"
+                      className="w-8 h-8 rounded-full"
                       src={session.user.image}
                       alt="User Photo"
                       width={40}
                       height={40}
                     />
                   ) : (
-                    <UserIcon className="w-6 h-6" /> // Tailwind Placeholder Icon
+                    <UserIcon className="w-6 h-6 text-gray-700 dark:text-white" />
                   )}
                 </button>
+
                 {/* Profile Dropdown Menu */}
                 <div
-                  className={`${isProfileOpen ? "block" : "hidden"} w-full`}
-                  id="user-menu"
+                  className={`${isProfileOpen ? "block" : "hidden"} z-50 my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow-sm dark:bg-gray-700 dark:divide-gray-600`}
                 >
                   <div className="px-4 py-3">
                     <span className="block text-sm text-gray-900 dark:text-white">
-                      {session.user?.displayName}
+                      {session.user?.name}
                     </span>
                     <span className="block text-sm text-gray-500 truncate dark:text-gray-400">
                       {session.user?.email}
@@ -106,20 +107,89 @@ export default function Header() {
               // Unauthenticated User - Show Sign-In Button
               <button
                 onClick={() => signIn("google")}
-                className="px-4 py-2 border flex gap-2 border-slate-200 dark:border-slate-700 rounded-lg text-slate-700 dark:text-slate-200 hover:border-slate-400 dark:hover:border-slate-500 hover:text-slate-900 dark:hover:text-slate-300 hover:shadow transition duration-150"
+                className="flex items-center justify-center w-8 h-8 border border-slate-200 dark:border-slate-700 rounded-full text-slate-700 dark:text-slate-200 hover:border-slate-400 dark:hover:border-slate-500 hover:text-slate-900 dark:hover:text-slate-300 hover:shadow transition duration-150"
               >
-                <Image
-                  className="w-6 h-6"
-                  src="https://www.svgrepo.com/show/475656/google-color.svg"
-                  loading="lazy"
-                  alt="google logo"
-                  height={75}
-                  width={75}
-                />
-                <span>Login with Google</span>
+                <UserIcon 
+                width={40}
+                height={40}
+                className="w-6 h-6 text-gray-700 dark:text-white" />
               </button>
             )}
           </div>
+
+          {/* Mobile Menu Button */}
+          <button
+            type="button"
+            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+            onClick={() => setIsMenuOpen(!isMenuOpen)}
+            aria-expanded={isMenuOpen ? "true" : "false"}
+          >
+            <span className="sr-only">Open main menu</span>
+            <svg
+              className="w-5 h-5"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 17 14"
+            >
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M1 1h15M1 7h15M1 13h15"
+              />
+            </svg>
+          </button>
+        </div>
+
+        {/* Mobile Menu (links) */}
+        <div
+          className={`${isMenuOpen ? "block" : "hidden"} md:hidden`}
+          id="navbar-user"
+        >
+          <ul className="flex flex-col space-y-4 p-4 bg-gray-50 md:bg-transparent dark:bg-gray-800 md:dark:bg-transparent">
+            <li>
+              <Link
+                href="/"
+                className="block py-2 px-3 text-blue-700 md:text-white rounded-sm md:bg-transparent md:hover:bg-transparent md:hover:text-blue-700 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white"
+              >
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="/about"
+                className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+              >
+                About
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="/services"
+                className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+              >
+                Services
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="/pricing"
+                className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+              >
+                Pricing
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="/contact"
+                className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+              >
+                Contact
+              </Link>
+            </li>
+          </ul>
         </div>
       </nav>
     </header>
--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -17,25 +17,28 @@ export default function SearchBar() {
   return (
     <>
       {/* Search Bar */}
-      <label htmlFor="search" aria-label="Search" className="ml-5">
-        <input
+      <div className="w-full relative">
+        <input 
           type="text"
           name="search"
           id="search"
           placeholder="Explore"
-          className="w-full h-full rounded-l-lg p-2 border"
+          className="block w-full p-2 pe-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
           onInput={updateSearch}
         />
-      </label>
+        <a
+          type="submit"
+          href={`/catalog${searchValue}`}
+          className="absolute inset-y-0 end-0 flex items-center pe-3"
+        >
+          <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+          </svg>
+        </a>
+      </div>
 
       {/* Submit Button */}
-      <a
-        type="submit"
-        href={`/catalog${searchValue}`}
-        className="w-[60px] h-[40px] bg-[#92DCE5] rounded-r-lg mr-5 flex items-center justify-center hover:brightness-90"
-      >
-        GO
-      </a>
+      
     </>
   );
 }
