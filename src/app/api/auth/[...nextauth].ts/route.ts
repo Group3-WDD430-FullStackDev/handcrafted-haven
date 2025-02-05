92bd09597c709b61848d992db3d4367e44a17c55
heathergibb <hrgibb@gmail.com>
2025-02-05 10:29:09 -0700

    delete extra folder

--- src/app/api/auth/[...nextauth].ts/route.ts
+++
@@ -1,8 +0,0 @@
-import NextAuth from "next-auth";
-import { authOptions } from "@/app/lib/auth";
-
-console.log("NextAuth handler is being loaded");
-
-const handler = NextAuth(authOptions);
-
-export { handler as GET, handler as POST };
