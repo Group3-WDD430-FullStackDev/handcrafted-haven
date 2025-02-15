dbcdbb9271150274d3f12a2f6b409aa8c0b3a026
heathergibb <hrgibb@gmail.com>
2025-02-14 20:04:45 -0700

    fixed seller image on product details page, added aria-label to reveiws

--- src/components/Cards/ProductDetailsCard.tsx
+++ src/components/Cards/ProductDetailsCard.tsx
@@ -114,9 +114,10 @@ const ProductDetailCard: React.FC<{ product: IProductDetailCard }> = ({
             <Image
               src={imageUrl}
               alt="product Image"
-              layout="fill"
-              objectFit="contain"
-              objectPosition="top"
+              fill
+              priority
+              sizes="(max-width: 1000px"
+              className="object-contain object-top"
             />
           </div>
         )}
@@ -155,7 +156,7 @@ const ProductDetailCard: React.FC<{ product: IProductDetailCard }> = ({
               alt="Sellers Image"
               width={100}
               height={100}
-              className="w-20 h-20 rounded-full"
+              className="w-20 h-20 rounded-full object-cover"
             />
           )}
         </div>
